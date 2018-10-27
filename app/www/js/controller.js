12743d8987810fd9a2e2d2cad087923b343f06ce
ZuGeTor <14806377+zugetor@users.noreply.github.com>
2018-10-28 06:21:04 +0700

    Add Map

--- app/www/js/controller.js
+++ app/www/js/controller.js
@@ -205,7 +205,7 @@ angular.module('starter.controllers', [])
 		}
 	})
 
-	.controller('selecttypeCtrl', function ($scope, $stateParams, database) {
+	.controller('selecttypeCtrl', function ($scope, $stateParams, $cordovaLaunchNavigator, database) {
 		$scope.allpet = database.get();
 		$scope.type = $stateParams.type;
 		$scope.pet2 = $scope.allpet["allpet"][$scope.type][$stateParams.id];		
@@ -306,3 +306,43 @@ angular.module('starter.controllers', [])
 
 	.controller('mypetlistCtrl', function ($scope) {
 	})
+	
+	.service('$cordovaLaunchNavigator', ['$q', function ($q) {
+		"use strict";
+
+		var $cordovaLaunchNavigator = {};
+		$cordovaLaunchNavigator.navigate = function (destination, options) {
+		  var q = $q.defer(),
+			isRealDevice = ionic.Platform.isWebView();
+
+		  if (!isRealDevice) {
+			q.reject("launchnavigator will only work on a real mobile device! It is a NATIVE app launcher.");
+		  } else {
+			try {
+
+			  var successFn = options.successCallBack || function () {
+				  },
+				errorFn = options.errorCallback || function () {
+				  },
+				_successFn = function () {
+				  successFn();
+				  q.resolve();
+				},
+				_errorFn = function (err) {
+				  errorFn(err);
+				  q.reject(err);
+				};
+
+			  options.successCallBack = _successFn;
+			  options.errorCallback = _errorFn;
+
+			  launchnavigator.navigate(destination, options);
+			} catch (e) {
+			  q.reject("Exception: " + e.message);
+			}
+		  }
+		  return q.promise;
+		};
+
+		return $cordovaLaunchNavigator;
+	}])
