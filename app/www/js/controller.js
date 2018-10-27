b2f5b29038e0618d86f78826684408b0ded2c3da
NeMo <friend-opp_2010@hotmail.com>
2018-10-28 00:20:45 +0700

    aaaa

--- app/www/js/controller.js
+++ app/www/js/controller.js
@@ -1,113 +1,113 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-})
-.controller('findFormCtrl', function($scope, $rootScope, $timeout) {
-	$scope.slideIndex = 1;
-	
-	$scope.showDivs = function(n){
-		var i;
-		var x = document.getElementsByClassName("mySlides");
-		if (n > x.length) {$scope.slideIndex = 1}    
-		if (n < 1) {$scope.slideIndex = x.length}
-		for (i = 0; i < x.length; i++) {
-			x[i].style.display = "none";  
-		}
-		x[$scope.slideIndex-1].style.display = "block"; 
-	};
-	$scope.plusDivs = function(n) {
-		$scope.showDivs($scope.slideIndex += n);
-	}
-	$scope.show = function() {
-		document.getElementById("ctrlbutton").classList.remove("hide");
-		$scope.plusDivs(0);
-	};
-})
+	.controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
+	})
+	.controller('findFormCtrl', function ($scope, $rootScope, $timeout) {
+		$scope.slideIndex = 1;
 
-.controller('HomeCtrl', function($scope, $rootScope, $interval, $ionicPlatform, logincheck) {
-	var tip = ["ถั่วแมคคาเดเมียจะทำให้กล้ามเนื้อสุนัขอ่อนแรง โดยจะส่งผลกับขาหลังของสุนัข อาจเป็นหนักถึงอัมพาต",
-	"ไข่ดิบจะทำให้สุนัขขาดไบโอติน ผลก็คือ ผิวจะแห้ง และเป็นเกล็ดขุยๆ ขนหลุดร่วง เจริญเติบโตช้ากว่าปกติ",
-	"ผักบุ้ง (Morning glory) เป็นโทษกับสุนัข",
-	"พลูด่าง (Golden pothos) หากสุนัขกินเข้าไปแล้ว จะทำให้เกิดการระคายเคืองภายในช่องปาก",
-	"สุนัขสามารถสื่อสารบอกอารมณ์ด้วยหางของมัน การกระดิกหางเป็นภาษากายที่กำลังบอกว่า ดีใจ",
-	"สุนัขส่วนใหญ่ไม่มีเอนไซม์ใช้ย่อยน้ำตาลแลคโตสในนมวัว จะทำให้เกิดท้องเสีย อาเจียน และเซื่องซึม",
-	"หัวหอมและกระเทียม จะไปทำลายเซลล์เม็ดเลือดแดง ส่งผลให้สุนัขเป็นโรคโลหิตจาง และโรคเลือดไหลไม่หยุด",
-	"ปรงสาคู (Cycas revoluta) เสี่ยงเป็นอันตรายต่อชีวิตสุนัข",
-	"การลูบไล้จะช่วยลดความดันโลหิตของแมวได้",
-	"แมวตั้งท้องประมาณ 62 - 65 วัน นับจากวันที่ผสมจนถึงวันคลอด",
-	"แมวมีความไวต่อแรงสั่นสะเทือนสูงมาก ดังนั้นแมวจะได้รับสัมผัสแรงสั่นสะเทือนแผ่นดินไหวได้ก่อนมนุษย์ 10 - 15 นาที",
-	"ปัสสาวะของแมวจะเรืองแสง เมื่อถูกส่งด้วยแสงแบล๊คไลท์",
-	"ปกติแมวจะไม่ร้องเสียงเหมืยวใส่แมวตัวอื่น จะร้องเสียงนี้ใส่คนเท่านั้น แต่จะทำเสียงครวญครางและเสียงขู่ใส่แมวตัวอื่น",
-	"แมวเพศเมียมักถนัดใช้มือขวา ส่วนแมวเพศผู้มักถนัดซ้าย",
-	"แมวไม่มีต่อมเหงื่อ ยกเว้นที่อุ้งเท้า",
-	"แมวทำเสียงต่างๆได้ประมาณ 100 เสียง ในขณะที่สุนัขทำได้แค่ 10 เสียง ",
-	"ถ้าแมวเอาหน้าเขาไปถูกับตัวคุณ แปลว่าเขายอมรับและแสดงความรักกับคุณ",
-	"หากเจ้าเหมียวเจอหน้าคุณแล้วพวกมันยกหางชี้ตรง ปลายหางแกว่งเล็กน้อย แสดงว่าพวกมันกำลังแสดงความรักต่อคุณ",
-	"การเคี้ยวเนื้อสดจะช่วยให้เหงือกและฟันของแมวมีสุขภาพที่ดีและแข็งแรงยิ่งขึ้น",
-	"วิธีการอุ้มกระต่าย ต้องทำให้รู้สึกผ่อนคลายโดยการลูบขนเบาๆ จากนั้นเอามือสอดเข้าไปใต้ท้องแล้วดึงเข้ามาแนบลำตัว",
-	"อย่าหิ้วหูกระต่ายเด็ดขาด!!",
-	"กระต่ายเป็นสัตว์ที่ชอบหลบซ่อนอยู่ในมุมมืด",
-	"ไม่ควรให้กระต่ายกินอาหารเม็ดเพียงอย่างเดียว",
-	"กระต่ายเป็นสัตว์ที่เครียดง่ายและชอบให้ลูบหัว",
-	"กระต่ายเป็นสัตว์สังคมชอบอยู่รวมกันหลายตัว",
-	"หนูแฮมสเตอร์ชอบกินไข่ต้มที่ต้มสุก",
-	"กระรอกมีการสะสมหรือซ่อนอาหารไว้กินในฤดูหนาว",
-	"ต้นบอนสี (Caladium) เป็นพืชที่มีพิษกับสุนัข หรือแมว"];
-
-	$scope.tipOf= "ช็อตโกแลต เป็นอาหารต้องห้ามของเหล่าสัตว์เลี้ยง";
-	$scope.randomtip= function () {
-		$scope.tipOf = tip[Math.floor(Math.random()*tip.length)];
-	}
-	$interval( function(){ $scope.randomtip(); }, 10000);
-	$scope.go = function () {		
-		logincheck.go();
-	}
-})
-
-.controller('LoginCtrl', function($scope, $state, logincheck) {
-	$scope.login = function () {		
-		logincheck.enter();
-		$state.go("app.home");
-	};	
-})
-
-.controller('ProfileCtrl', function($scope, $state, logincheck) {
-	$scope.logout = function () {		
-		logincheck.leave();
-	};
-})
-
-.factory('logincheck', function($rootScope, $state){
-     
-     var hasLogin = false;
-
-     return {
-        go: function(){
-			if(!hasLogin){
-				$state.go("app.login");
-			}else{
-				$state.go("app.profile");
+		$scope.showDivs = function (n) {
+			var i;
+			var x = document.getElementsByClassName("mySlides");
+			if (n > x.length) { $scope.slideIndex = 1 }
+			if (n < 1) { $scope.slideIndex = x.length }
+			for (i = 0; i < x.length; i++) {
+				x[i].style.display = "none";
 			}
-        },
-        enter : function(){
-			hasLogin = true;			
-        },
-		leave : function(){
-			hasLogin = false;			
-        }		
-     }
+			x[$scope.slideIndex - 1].style.display = "block";
+		};
+		$scope.plusDivs = function (n) {
+			$scope.showDivs($scope.slideIndex += n);
+		}
+		$scope.show = function () {
+			document.getElementById("ctrlbutton").classList.remove("hide");
+			$scope.plusDivs(0);
+		};
+	})
 
-})
+	.controller('HomeCtrl', function ($scope, $rootScope, $interval, $ionicPlatform, logincheck) {
+		var tip = ["ถั่วแมคคาเดเมียจะทำให้กล้ามเนื้อสุนัขอ่อนแรง โดยจะส่งผลกับขาหลังของสุนัข อาจเป็นหนักถึงอัมพาต",
+			"ไข่ดิบจะทำให้สุนัขขาดไบโอติน ผลก็คือ ผิวจะแห้ง และเป็นเกล็ดขุยๆ ขนหลุดร่วง เจริญเติบโตช้ากว่าปกติ",
+			"ผักบุ้ง (Morning glory) เป็นโทษกับสุนัข",
+			"พลูด่าง (Golden pothos) หากสุนัขกินเข้าไปแล้ว จะทำให้เกิดการระคายเคืองภายในช่องปาก",
+			"สุนัขสามารถสื่อสารบอกอารมณ์ด้วยหางของมัน การกระดิกหางเป็นภาษากายที่กำลังบอกว่า ดีใจ",
+			"สุนัขส่วนใหญ่ไม่มีเอนไซม์ใช้ย่อยน้ำตาลแลคโตสในนมวัว จะทำให้เกิดท้องเสีย อาเจียน และเซื่องซึม",
+			"หัวหอมและกระเทียม จะไปทำลายเซลล์เม็ดเลือดแดง ส่งผลให้สุนัขเป็นโรคโลหิตจาง และโรคเลือดไหลไม่หยุด",
+			"ปรงสาคู (Cycas revoluta) เสี่ยงเป็นอันตรายต่อชีวิตสุนัข",
+			"การลูบไล้จะช่วยลดความดันโลหิตของแมวได้",
+			"แมวตั้งท้องประมาณ 62 - 65 วัน นับจากวันที่ผสมจนถึงวันคลอด",
+			"แมวมีความไวต่อแรงสั่นสะเทือนสูงมาก ดังนั้นแมวจะได้รับสัมผัสแรงสั่นสะเทือนแผ่นดินไหวได้ก่อนมนุษย์ 10 - 15 นาที",
+			"ปัสสาวะของแมวจะเรืองแสง เมื่อถูกส่งด้วยแสงแบล๊คไลท์",
+			"ปกติแมวจะไม่ร้องเสียงเหมืยวใส่แมวตัวอื่น จะร้องเสียงนี้ใส่คนเท่านั้น แต่จะทำเสียงครวญครางและเสียงขู่ใส่แมวตัวอื่น",
+			"แมวเพศเมียมักถนัดใช้มือขวา ส่วนแมวเพศผู้มักถนัดซ้าย",
+			"แมวไม่มีต่อมเหงื่อ ยกเว้นที่อุ้งเท้า",
+			"แมวทำเสียงต่างๆได้ประมาณ 100 เสียง ในขณะที่สุนัขทำได้แค่ 10 เสียง ",
+			"ถ้าแมวเอาหน้าเขาไปถูกับตัวคุณ แปลว่าเขายอมรับและแสดงความรักกับคุณ",
+			"หากเจ้าเหมียวเจอหน้าคุณแล้วพวกมันยกหางชี้ตรง ปลายหางแกว่งเล็กน้อย แสดงว่าพวกมันกำลังแสดงความรักต่อคุณ",
+			"การเคี้ยวเนื้อสดจะช่วยให้เหงือกและฟันของแมวมีสุขภาพที่ดีและแข็งแรงยิ่งขึ้น",
+			"วิธีการอุ้มกระต่าย ต้องทำให้รู้สึกผ่อนคลายโดยการลูบขนเบาๆ จากนั้นเอามือสอดเข้าไปใต้ท้องแล้วดึงเข้ามาแนบลำตัว",
+			"อย่าหิ้วหูกระต่ายเด็ดขาด!!",
+			"กระต่ายเป็นสัตว์ที่ชอบหลบซ่อนอยู่ในมุมมืด",
+			"ไม่ควรให้กระต่ายกินอาหารเม็ดเพียงอย่างเดียว",
+			"กระต่ายเป็นสัตว์ที่เครียดง่ายและชอบให้ลูบหัว",
+			"กระต่ายเป็นสัตว์สังคมชอบอยู่รวมกันหลายตัว",
+			"หนูแฮมสเตอร์ชอบกินไข่ต้มที่ต้มสุก",
+			"กระรอกมีการสะสมหรือซ่อนอาหารไว้กินในฤดูหนาว",
+			"ต้นบอนสี (Caladium) เป็นพืชที่มีพิษกับสุนัข หรือแมว"];
 
-.controller('selecttypeCtrl', function($scope, $state, logincheck) {
-	
-})
+		$scope.tipOf = "ช็อตโกแลต เป็นอาหารต้องห้ามของเหล่าสัตว์เลี้ยง";
+		$scope.randomtip = function () {
+			$scope.tipOf = tip[Math.floor(Math.random() * tip.length)];
+		}
+		$interval(function () { $scope.randomtip(); }, 10000);
+		$scope.go = function () {
+			logincheck.go();
+		}
+	})
+
+	.controller('LoginCtrl', function ($scope, $state, logincheck) {
+		$scope.login = function () {
+			logincheck.enter();
+			$state.go("app.home");
+		};
+	})
+
+	.controller('ProfileCtrl', function ($scope, $state, logincheck) {
+		$scope.logout = function () {
+			logincheck.leave();
+		};
+	})
+
+	.factory('logincheck', function ($rootScope, $state) {
+
+		var hasLogin = false;
+
+		return {
+			go: function () {
+				if (!hasLogin) {
+					$state.go("app.login");
+				} else {
+					$state.go("app.profile");
+				}
+			},
+			enter: function () {
+				hasLogin = true;
+			},
+			leave: function () {
+				hasLogin = false;
+			}
+		}
+
+	})
+
+	.controller('selecttypeCtrl', function ($scope, $state, logincheck) {
+
+	})
 
 
-.controller('selectpetsCtrl', function($scope, $state, $stateParams, logincheck) {
-	$scope.allpet={
-		"allpet": {
-			"dog": [{
+	.controller('selectpetsCtrl', function ($scope, $state, $stateParams, logincheck) {
+		$scope.allpet = {
+			"allpet": {
+				"dog": [{
 					"name": "โกโก้",
 					"type": "สุนัข",
 					"age": 3,
@@ -115,7 +115,7 @@ angular.module('starter.controllers', [])
 					"description": "สีน้ำตาล ฟันหลอ 1 ซี่ ชอบกินเนื้อย่าง",
 					"breed": "บางแก้วผสมไทยหลังอาน",
 					"tag": ["ขี้เล่น", "ขี้อ้อน"],
-					"imgurl":["img/cocoa.jpg","img/cocoa2.jpg"]
+					"imgurl": ["img/cocoa.jpg", "img/cocoa2.jpg"]
 				},
 				{
 					"name": "ลัคกี้",
@@ -124,7 +124,8 @@ angular.module('starter.controllers', [])
 					"gender": "Female",
 					"description": "ขนสีขาว ไม่ชอบที่มืด",
 					"breed": "พุดเดิ้ล",
-					"tag": ["ขี้เล่น", "ขี้อ้อน"]
+					"tag": ["ขี้เล่น", "ขี้อ้อน"],
+					"imgurl": ["img/Lucky.jpg"]
 				},
 				{
 					"name": "ชิโร่",
@@ -133,7 +134,8 @@ angular.module('starter.controllers', [])
 					"gender": "Male",
 					"description": "สีเนื้อ ขนมันเงา ชอบเล่นจานร่อน",
 					"breed": "อกิตะ",
-					"tag": ["ขี้เล่น"]
+					"tag": ["ขี้เล่น"],
+					"imgurl": ["img/shiro.jpg"]
 				},
 				{
 					"name": "โครอน",
@@ -142,7 +144,8 @@ angular.module('starter.controllers', [])
 					"gender": "Female",
 					"description": "สีน้ำตาล ชอบวิ่งเล่น",
 					"breed": "คอลลี่",
-					"tag": ["ขี้เล่น", "ขี้อ้อน"]
+					"tag": ["ขี้เล่น", "ขี้อ้อน"],
+					"imgurl": ["img/coron.jpg"]
 				},
 				{
 					"name": "เฮงเฮง",
@@ -151,7 +154,8 @@ angular.module('starter.controllers', [])
 					"gender": "Male",
 					"description": "สีขาวดำ",
 					"breed": "-",
-					"tag": ["ขี้เซา"]
+					"tag": ["ขี้เซา"],
+					"imgurl": ["img/hengheng.jpg"]
 				},
 				{
 					"name": "ลาเต้",
@@ -162,7 +166,8 @@ angular.module('starter.controllers', [])
 					"breed": "โกลเด้น",
 					"tag": [
 						"ขี้เล่น"
-					]
+					],
+					"imgurl": ["img/ล้าเต้.jpg", "img/ลาเต้2.jpg"]
 				},
 				{
 					"name": "ตาล",
@@ -174,7 +179,8 @@ angular.module('starter.controllers', [])
 					"tag": [
 						"ขี้กลัว",
 						"ขี้อ้อน"
-					]
+					],
+					"imgurl": ["img/ตาล.jpg", "img/ตาล2.jpg"]
 				},
 				{
 					"name": "บูบู้",
@@ -186,7 +192,8 @@ angular.module('starter.controllers', [])
 					"tag": [
 						"ขี้เล่น",
 						"ก้าวร้าว"
-					]
+					],
+					"imgurl": ["img/บูบู้.jpg", "img/บูบู้2.jpg"]
 				},
 				{
 					"name": "ลินดา",
@@ -198,7 +205,8 @@ angular.module('starter.controllers', [])
 					"tag": [
 						"ขี้เล่น",
 						"ขี้อ้อน"
-					]
+					],
+					"imgurl": ["img/ลินดา.jpg", "img/ลินดา2.jpg"]
 				},
 				{
 					"name": "ปุ๊โกะ",
@@ -211,18 +219,20 @@ angular.module('starter.controllers', [])
 						"ขี้เล่น",
 						"ขี้อ้อน",
 						"ขี้เซา"
-					]
+					],
+					"imgurl": ["img/ปุ๊โกะ.jpg", "img/ปุ๊โกะ.jpg"]
 				}
-	
-			],
-			"cat": [{
+
+				],
+				"cat": [{
 					"name": "สิริ",
 					"type": "แมว",
 					"age": 2,
 					"gender": "Female",
 					"description": "สามสี ส้ม ขาว ดำ ชอบกินปลาทูทอด ขี้อ้อน กลัวหมา",
 					"breed": "สามสี",
-					"tag": ["ขี้เล่น", "ขี้อ้อน"]
+					"tag": ["ขี้เล่น", "ขี้อ้อน"],
+					"imgurl": ["img/siri.jpg"]
 				},
 				{
 					"name": "แฮปปี้",
@@ -231,7 +241,8 @@ angular.module('starter.controllers', [])
 					"gender": "Male",
 					"description": "สีฟ้า ชอบนอน",
 					"breed": "บริติชขนสั้น",
-					"tag": ["ขี้เซา"]
+					"tag": ["ขี้เซา"],
+					"imgurl": ["img/happy.jpg"]
 				},
 				{
 					"name": "มาจัง",
@@ -240,7 +251,8 @@ angular.module('starter.controllers', [])
 					"gender": "Female",
 					"description": "สีขาวอมส้ม ชอบนั่งหน้าโทรทัศน์",
 					"breed": "มันช์กิ้น",
-					"tag": ["ขี้เล่น", "ขี้อ้อน"]
+					"tag": ["ขี้เล่น", "ขี้อ้อน"],
+					"imgurl": ["img/maa.jpg"]
 				},
 				{
 					"name": "จี้จัง",
@@ -249,7 +261,8 @@ angular.module('starter.controllers', [])
 					"gender": "Female",
 					"description": "สีขาว ชอบเล่นของเล่น",
 					"breed": "สิงหะปุระ",
-					"tag": ["ขี้อ้อน"]
+					"tag": ["ขี้อ้อน"],
+					"imgurl": ["img/chi.jpg"]
 				},
 				{
 					"name": "โลจัง",
@@ -258,17 +271,19 @@ angular.module('starter.controllers', [])
 					"gender": "Female",
 					"description": "สีฟ้าเจือเงิน ชอบเล่นของเล่น",
 					"breed": "รัสเซียนบลู",
-					"tag": ["ขี้อ้อน"]
+					"tag": ["ขี้อ้อน"],
+					"imgurl": ["img/ro.jpg", "img/ro2.jpg"]
 				}
-			],
-			"bird": [{
+				],
+				"bird": [{
 					"name": "คิริ",
 					"type": "นก",
 					"age": 3,
 					"gender": "Male",
 					"description": "สีเหลือง ขอบร้องเพลง",
 					"breed": "คีรีบูน",
-					"tag": ["ขี้เล่น"]
+					"tag": ["ขี้เล่น"],
+					"imgurl": ["img/kiri.jpg"]
 				},
 				{
 					"name": "นิจิ",
@@ -277,7 +292,8 @@ angular.module('starter.controllers', [])
 					"gender": "Male",
 					"description": "ชอบเกาะตามขอบตู้หรือเตียง",
 					"breed": "เยลโล่ไซต์คอนนัว",
-					"tag": ["ขี้กลัว"]
+					"tag": ["ขี้กลัว"],
+					"imgurl": ["img/niji.jpg"]
 				},
 				{
 					"name": "ซันนี่",
@@ -286,7 +302,8 @@ angular.module('starter.controllers', [])
 					"gender": "Female",
 					"description": "ชอบบินมาเกาะ",
 					"breed": "ซันคอนัว",
-					"tag": ["ขี้้อ้อน"]
+					"tag": ["ขี้้อ้อน"],
+					"imgurl": ["img/sunny.jpg"]
 				},
 				{
 					"name": "ยูกิ",
@@ -295,7 +312,8 @@ angular.module('starter.controllers', [])
 					"gender": "Female",
 					"description": "ชอบเล่นไล่จับ",
 					"breed": "นกกระตั้ว",
-					"tag": ["ขี้เล่น"]
+					"tag": ["ขี้เล่น"],
+					"imgurl": ["img/yuki.jpg"]
 				},
 				{
 					"name": "กิ๊ก",
@@ -306,28 +324,31 @@ angular.module('starter.controllers', [])
 					"breed": "แก้ว",
 					"tag": [
 						"ขี้เล่น"
-					]
+					],
+					"imgurl": ["img/กิ๊ก.jpg", "img/กิ๊ก2.jpg"]
 				}
-			],
-			"mouse": [{
-				"name": "พายุ",
-				"type": "แฮมสเตอร์",
-				"age": 1,
-				"gender": "Male",
-				"description": "ชอบวิ่ง ชอบกินธัญพืช ไม่ชอบอยู่เฉยๆ",
-				"breed": "-",
-				"tag": [
-					"ขี้เล่น"
-				]
-			}],
-			"rabbit": [{
+				],
+				"mouse": [{
+					"name": "พายุ",
+					"type": "แฮมสเตอร์",
+					"age": 1,
+					"gender": "Male",
+					"description": "ชอบวิ่ง ชอบกินธัญพืช ไม่ชอบอยู่เฉยๆ",
+					"breed": "-",
+					"tag": [
+						"ขี้เล่น"
+					],
+					"imgurl": ["img/พายุ.jpg", "img/พายุ2.jpg"]
+				}],
+				"rabbit": [{
 					"name": "ทิปปี้",
 					"type": "กระต่าย",
 					"age": 4,
 					"gender": "Female",
 					"description": "สีขาว ขนนุ่มพู",
 					"breed": "แองโกล่า",
-					"tag": ["ขี้เล่น", "ขี้อ้อน"]
+					"tag": ["ขี้เล่น", "ขี้อ้อน"],
+					"imgurl": ["img/tippy.jpg"]
 				},
 				{
 					"name": "อันโกะ",
@@ -336,7 +357,8 @@ angular.module('starter.controllers', [])
 					"gender": "Male",
 					"description": "สีดำ ชอบนั่งอยู่นิ่งๆ",
 					"breed": "แองโกล่า",
-					"tag": ["ขี้เซา"]
+					"tag": ["ขี้เซา"],
+					"imgurl": ["img/anko.jpg"]
 				},
 				{
 					"name": "ชิรอน",
@@ -345,7 +367,8 @@ angular.module('starter.controllers', [])
 					"gender": "Female",
 					"description": "สีขาว ชอบเล่นในกล่อง",
 					"breed": "อิงลิซ ล็อป",
-					"tag": ["ขี้เล่น"]
+					"tag": ["ขี้เล่น"],
+					"imgurl": ["img/shiron.jpg"]
 				},
 				{
 					"name": "โรโกะ",
@@ -354,7 +377,8 @@ angular.module('starter.controllers', [])
 					"gender": "Female",
 					"description": "สีขาว ตื่นคนแปลกหน้า",
 					"breed": "เท็ดดี้แบร์ ",
-					"tag": ["ขี้กลัว"]
+					"tag": ["ขี้กลัว"],
+					"imgurl": ["img/roko.jpg"]
 				},
 				{
 					"name": "เจอรี่",
@@ -363,7 +387,8 @@ angular.module('starter.controllers', [])
 					"gender": "Male",
 					"description": "สีน้ำตาาล ตื่นเสียงสุนัข",
 					"breed": "เจอร์รี่วู๊ดดี้ ",
-					"tag": ["ขี้กลัว", "ขี้อ้อน"]
+					"tag": ["ขี้กลัว", "ขี้อ้อน"],
+					"imgurl": ["img/jerry.jpg"]
 				},
 				{
 					"name": "วินดี้",
@@ -374,21 +399,23 @@ angular.module('starter.controllers', [])
 					"breed": "ฮอลแลนด์ลอป",
 					"tag": [
 						"ขี้เซา"
-					]
+					],
+					"imgurl": ["img/วินดี้.jpg"]
 				},
 				{
-					"name": "มิน่า",
+					"name": "เท็ดดี้",
 					"type": "กระต่าย",
 					"age": 3,
-					"gender": "Female",
+					"gender": "Male",
 					"description": "สีขาว ชอบวิ่ง ซน",
 					"breed": "เท็ดดี้แบร์",
 					"tag": [
 						"ขี้เล่น"
-					]
+					],
+					"imgurl": ["img/เท็ดดี้.jpg", "img/เท็ดดี้2.jpg", "img/เท็ดดี้3.jpg"]
 				}
-			],
-			"other": [{
+				],
+				"other": [{
 					"name": "มีมี่",
 					"type": "เม่นแคระ",
 					"age": 2,
@@ -398,7 +425,8 @@ angular.module('starter.controllers', [])
 					"tag": [
 						"ขี้กลัว",
 						"ขี้เซา"
-					]
+					],
+					"imgurl": ["img/มีมี่.jpg", "img/มีมี่2.jpg"]
 				},
 				{
 					"name": "โซจิโร่",
@@ -407,13 +435,14 @@ angular.module('starter.controllers', [])
 					"gender": "Male",
 					"description": "ชอบกินแอปเปิล",
 					"breed": "-",
-					"tag": ["ขี้เซา", "ขี้อ้อน"]
+					"tag": ["ขี้เซา", "ขี้อ้อน"],
+					"imgurl": ["img/cocoa.jpg", "img/cocoa2.jpg"]
 				}
-			]
-		}
-	};
-	$scope.type = $stateParams.type;
-	$scope.pet = $scope.allpet["allpet"][$scope.type]
-})
+				]
+			}
+		};
+		$scope.type = $stateParams.type;
+		$scope.pet = $scope.allpet["allpet"][$scope.type]
+	})
 
 
